f7374a5b9e4aad11a0c7fdc5459ecfa59b405eeb
James Kontargyris <james@jameskontargyris.co.uk>
2015-04-17 11:11:41 +0000

    Added company name field to White House Dinner form

--- app/Http/Requests/WhiteHouseDinnerRequest.php
+++ app/Http/Requests/WhiteHouseDinnerRequest.php
@@ -23,23 +23,25 @@ class WhiteHouseDinnerRequest extends Request
     public function rules()
     {
         return [
-            'first_name' => 'required',
-            'last_name'  => 'required',
-            'email'      => 'required|email',
-            'telephone'  => 'required',
-            'invited_by' => 'required'
+            'first_name'   => 'required',
+            'last_name'    => 'required',
+            'company_name' => 'required',
+            'email'        => 'required|email',
+            'telephone'    => 'required',
+            'invited_by'   => 'required'
         ];
     }
 
     public function messages()
     {
         return [
-            'first_name.required' => 'Please enter your first name.',
-            'last_name.required'  => 'Please enter your last name.',
-            'email.required'      => 'Please enter your email address.',
-            'email.email'         => 'Please enter a valid email address.',
-            'telephone.required'  => 'Please enter your mobile/cell number.',
-            'invited_by.required' => 'Please enter the name of the Fipra Network member who invited you.'
+            'first_name.required'   => 'Please enter your first name.',
+            'last_name.required'    => 'Please enter your last name.',
+            'company_name.required' => 'Please enter the name of your company.',
+            'email.required'        => 'Please enter your email address.',
+            'email.email'           => 'Please enter a valid email address.',
+            'telephone.required'    => 'Please enter your mobile/cell number.',
+            'invited_by.required'   => 'Please enter the name of the Fipra Network member who invited you.'
         ];
     }
 
